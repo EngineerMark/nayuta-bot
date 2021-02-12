2d3c919c127bd811f40603c8cc9885bb2443ac49
Mark Meijer <engineermarkpro@gmail.com>
2021-02-12 23:44:02 +0100

    project created

---
+++ nayuta/nayuta/Program.cs
@@ -0,0 +1,34 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nayuta
+{
+    class Program
+    {
+        private DiscordSocketClient _client;
+
+        public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+            var token = "ODA5OTEyMTUzNTgyNzMxMzI0.YCb_eA.E6W2dgDkUrcO1ptZvlnPMX2yo3w";
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
