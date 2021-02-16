223bc65e8b9f54920b1e7d79bd944c5e19ff7d5f
Mark Meijer <engineermarkpro@gmail.com>
2021-02-16 14:04:19 +0100

    db disabled

--- nayuta/nayuta/DatabaseManager.cs
+++ nayuta/nayuta/DatabaseManager.cs
@@ -14,13 +14,13 @@ namespace nayuta
         public DatabaseManager()
         {
             string connstring = string.Format("Server={0}; database={1}; UID={2}; password={3}", DB_SERVER, DB_NAME, DB_USER, DB_PASSWORD);
-            dbConnection = new MySqlConnection(connstring);
-            dbConnection.Open();
+            //dbConnection = new MySqlConnection(connstring);
+            //dbConnection.Open();
         }
 
         public void Close()
         {
-            dbConnection.Close();
+            //dbConnection.Close();
         }
     }
 }
