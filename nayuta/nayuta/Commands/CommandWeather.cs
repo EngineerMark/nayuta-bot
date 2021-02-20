780675d6a4f22980f98c685da6a66479a5994b7d
Mark Meijer <engineermarkpro@gmail.com>
2021-02-20 23:10:14 +0100

    weather command base added

---
+++ nayuta/nayuta/Commands/CommandWeather.cs
@@ -0,0 +1,17 @@
+﻿using Discord.WebSocket;
+
+namespace nayuta.Commands
+{
+    public class CommandWeather : Command
+    {
+        public CommandWeather() : base("weather", "Fetch the weather for given area")
+        {
+            InputValue = true;
+        }
+
+        public override object CommandHandler(SocketMessage socketMessage, string input, CommandArguments arguments)
+        {
+            return "Not implemented yet.";
+        }
+    }
+}
